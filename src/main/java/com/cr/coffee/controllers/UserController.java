08cb86279750c8dab7b8e3cd01a9c86b2c953b31
christianruest <christian.ruest@hotmail.com>
2019-08-03 18:56:38 +0200

    added cross-origin to user controller

--- src/main/java/com/cr/coffee/controllers/UserController.java
+++ src/main/java/com/cr/coffee/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.cr.coffee.models.UserModel;
 import com.cr.coffee.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
